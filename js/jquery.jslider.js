f294a6734b602e3840b1ec501caefc3bec59e244
wnt111 <515505920@qq.com>
2021-06-11 22:50:08 +0800

    add wnt111

---
+++ js/jquery.jslider.js
@@ -0,0 +1,61 @@
+/****************************************************************
+ *																*		
+ * 						      							*
+ *                        							*
+ *       		  努力创建完善、持续更新插件以及模板			*
+ * 																*
+****************************************************************/
+/*
+* JuheSlider
+* Author:JONEDOO
+* Http://www.juhe.cn
+*/
+(function($){
+	$.fn.jSlider = function(options){
+		var defaults = {
+			pause:3000,
+			fadeOutTime:500,
+			fadeInTime:500,
+			naviSlider:'naviSlider'
+		}
+		
+		var options = $.extend(defaults, options);
+		
+		var obj = $(this);
+
+		obj.find('li:first').show();	//鏄剧ず绗竴涓猧tem
+		
+		var itemCount = obj.children('li').length;
+		
+		var naviObj=$("#"+options.naviSlider);
+		
+		var i=0;	//鍒濆鍖�
+		
+		naviObj.children('li').click(function(){			
+			//var index = $(this).text();
+		 	var index =$(this).attr("sIndex");
+			i = parseInt(index)-2;			
+			showSlider();		
+		})
+		
+		
+		
+		var showSlider = function(){
+			
+			var next =i+1;
+			if(i >= (itemCount-1)){
+				i=-1;	
+				next=0;						
+			}
+			obj.children('li').filter(":visible").fadeOut(options.fadeOutTime).parent().children().eq(next).fadeIn(options.fadeInTime);
+			
+			naviObj.children('li').removeClass('on').eq(next).addClass('on');
+		
+			i = i+1;
+		}
+		
+		var interval = setInterval(function(){ 
+			showSlider();
+		}, options.pause);
+	}
+})(jQuery);console.log("\u002f\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u000d\u000a\u0020\u002a\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u0009\u0009\u000d\u000a\u0020\u002a\u0020\u0009\u0009\u0009\u0009\u0009\u0009\u0020\u0020\u0020\u0020\u0020\u0020\u4ee3\u7801\u5e93\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0077\u0077\u0077\u002e\u0064\u006d\u0061\u006b\u0075\u002e\u0063\u006f\u006d\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0009\u0009\u0020\u0020\u52aa\u529b\u521b\u5efa\u5b8c\u5584\u3001\u6301\u7eed\u66f4\u65b0\u63d2\u4ef6\u4ee5\u53ca\u6a21\u677f\u0009\u0009\u0009\u002a\u000d\u000a\u0020\u002a\u0020\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u0009\u002a\u000d\u000a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002a\u002f");
